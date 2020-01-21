29befc20dc7db86da21fd8a301d21582191b0e7e
daisy-world <lipsapatra.daisy1992@gmail.com>
2020-01-21 11:12:01 +0530

    first

---
+++ src/com/example/DateConversion.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import java.util.Date;
+
+public class DateConversion {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("inside main..");
+		
+		getISOFromDate(new Date());
+		
+		System.out.println("date in iso string .... "   + getISOFromDate(new Date())  );
+		
+	}
+	
+	public static String getISOFromDate(Date date){
+		String dateString = null;
+		if (null != date) {
+			
+			dateString = date.toInstant().toString();
+			return dateString;
+		}
+		
+		return dateString;
+	}
+
+}
